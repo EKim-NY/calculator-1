6c01c7a0b70726a574583c2b5ab902cf795a3745
Eren Kim <erenkim@gmail.com>
2020-04-29 16:21:08 -0400

    Code cleanup: Removed float() in return line. Changed var name sum to total in add function

--- arithmetic.py
+++ arithmetic.py
@@ -3,22 +3,22 @@
 
 def add(num1, num2):
     """Return the sum of the two inputs."""
-    sum = num1 + num2 
-    return float(sum)
+    total = num1 + num2 
+    return total
 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
 
     difference = num1 - num2
-    return float(difference)
+    return difference
 
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
 
     product = num1 * num2
-    return float(product)
+    return product
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
